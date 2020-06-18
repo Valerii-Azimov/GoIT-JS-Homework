14c829fde1ca0aa69b6551a93f6f08e51f113cb6
unknown <azimov3.9.1991@gmail.com>
2020-06-18 15:32:26 +0300

    modul 8

---
+++ Modul 8/src/index.js
@@ -0,0 +1,51 @@
+import users from './gallery-items.js';
+
+// console.table(users);
+const galleryRef = document.querySelector('.gallery');
+const modalRef = document.querySelector('.lightbox');
+const lightboxImageRef = document.querySelector('.lightbox__image');
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+
+galleryRef.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+// console.dir(lightboxImageRef.src);
+
+const createGalery = user => {
+  const conteinerRef = document.createElement('li');
+  conteinerRef.classList.add('gallery__item');
+
+  const linkRef = document.createElement('a');
+  linkRef.classList.add('gallery__link');
+  //   linkRef.href = user.original;
+
+  const imgRef = document.createElement('img');
+  imgRef.classList.add('gallery__image');
+  imgRef.src = user.preview;
+  imgRef.dataSource = user.original;
+  imgRef.alt = user.description;
+
+  conteinerRef.append(linkRef);
+  linkRef.append(imgRef);
+  return conteinerRef;
+};
+
+const galleryImages = users.map(user => createGalery(user));
+galleryRef.append(...galleryImages);
+
+function openModal(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  modalRef.classList.add('is-open');
+  lightboxImageRef.src = event.target.dataSource;
+  console.log(lightboxImageRef.src);
+}
+
+function closeModal(event) {
+  modalRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+  console.log(lightboxImageRef.src);
+}
